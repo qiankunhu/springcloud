1041e01a4fd2e7ef6dd7da1f6a3ab5e98e80fd64
huqiankun <1390284004@qq.com>
2020-05-19 09:40:00 +0800

    修改 pom 聚合

--- springcloud-provider/src/main/java/com/hqk/springcloudprovider/SpringcloudProviderApplication.java
+++ springcloud-provider/src/main/java/com/hqk/springcloudprovider/SpringcloudProviderApplication.java
@@ -3,9 +3,11 @@ package com.hqk.springcloudprovider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient //服务注册
+@EnableFeignClients //启用feign进行远程调用
 public class SpringcloudProviderApplication {
 
     public static void main(String[] args) {
